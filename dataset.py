a540a19391c512e3e762539d23950a29459a4a2d
lingji-yidong <75744976+lingji-yidong@users.noreply.github.com>
2023-03-01 14:17:14 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -20,3 +20,23 @@ class TextDataset(Dataset):
         label = self.labels[idx]
         inputs = self.model_tokenizer(text, padding=True, truncation=True, return_tensors='pt')
         return {'input_ids': inputs['input_ids'][0], 'attention_mask': inputs['attention_mask'][0], 'label': torch.tensor(label)}
+
+    
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+class TextDataset(Dataset):
+    def __init__(self, file_path):
+        self.data = pd.read_excel(file_path)  # Load data from xlsx file
+        self.labels = self.data['labels'].unique()  # Get unique label values
+        self.label_to_id = {label: i for i, label in enumerate(self.labels)}  # Map labels to integers
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        label = self.data.loc[index, 'labels']
+        text = self.data.loc[index, 'text']
+        label_id = self.label_to_id[label]  # Convert label to integer
+        return {'text': text, 'label': label_id}
