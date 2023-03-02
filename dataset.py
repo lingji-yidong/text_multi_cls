1a21c331d1402ef59ea3a8debe405d865c4bb117
lingji-yidong <75744976+lingji-yidong@users.noreply.github.com>
2023-03-02 16:21:15 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -40,3 +40,44 @@ class TextDataset(Dataset):
         text = self.data.loc[index, 'text']
         label_id = self.label_to_id[label]  # Convert label to integer
         return {'text': text, 'label': label_id}
+    
+    
+import torch
+from torch.utils.data import DataLoader, Dataset
+from sklearn.model_selection import train_test_split
+
+# Define a custom dataset class
+class TextDataset(Dataset):
+    def __init__(self, texts, labels, tokenizer):
+        self.texts = texts
+        self.labels = labels
+        self.tokenizer = tokenizer
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        text = self.texts[idx]
+        label = self.labels[idx]
+        tokenized_text = self.tokenizer(text)
+        return tokenized_text, label
+
+# Define the texts and labels
+texts = ["this is a sample text", "this is another sample text", "yet another sample text", "one more sample text"]
+labels = [0, 1, 0, 1]
+
+# Split the data into training and validation sets
+train_texts, val_texts, train_labels, val_labels = train_test_split(texts, labels, test_size=0.2, random_state=42)
+
+# Define a tokenizer function
+def tokenizer(text):
+    return text.split()
+
+# Create instances of the TextDataset class for the training and validation sets
+train_dataset = TextDataset(train_texts, train_labels, tokenizer)
+val_dataset = TextDataset(val_texts, val_labels, tokenizer)
+
+# Define dataloaders for training and validation sets
+train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=2, shuffle=False)
+
